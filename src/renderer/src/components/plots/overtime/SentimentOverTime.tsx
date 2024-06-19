d1867d7954ad9dc6f27dec4e117bd4d15a48be5d
Juan Andrés Losauro <losaurojuan@gmail.com>
2024-06-19 17:35:56 -0300

    sprint 19/6

--- src/renderer/src/components/plots/overtime/SentimentOverTime.tsx
+++ src/renderer/src/components/plots/overtime/SentimentOverTime.tsx
@@ -3,7 +3,7 @@ import SentimentPieChart from './SentimentPieChart'
 
 function SentimentOverTime() {
   return (
-    <div className="bg-white p-5">
+    <div className="bg-white p-5 rounded-xl">
       <h1 className="text-indigo-950 text-xl mb-5 font-semibold">Sentiment Over Time</h1>
       <div className="flex flex-row items-center">
         <SentimentPercentChart />
